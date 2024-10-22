43b57a7d7b3ee56f3605146c59ec2a7e56f10cc0
Danielium <Lapshin_danila73@mail.ru>
2024-10-22 15:33:39 +0400

    new api key

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.types import Message
 from fal import process_images  # Импортируем функцию обработки изображений с нейросетью
 
-os.environ["FAL_KEY"] = "6707e945-df15-41e3-a458-5ba2b2728e43:bf9e5aa09432a399fe4865a17a2f6e55"
+os.environ["FAL_KEY"] = "51e1e97d-bde2-476e-b330-92f4ea45a257:c0a9de151ad33de6c57d37b319e9676c"
 BOT_TOKEN = '7829353526:AAE3kK88AJD81DrIIdkx5sBpP_uAD3d7QRw'
 
 start = 'Привет! Этот бот поможет вам определить, как будет выглядеть на вас выбранная одежда. Просто отправьте мне ' \
